7bd855b0079160d933d384f18bc9430cbcd197c3
Muhammad Talha <talhanaeem347@gmail,coom>
2023-05-19 11:41:43 +0500

    signup bloc with validation

---
+++ lib/bloc/signup/signup_event.dart
@@ -0,0 +1,68 @@
+
+part of 'signup_bloc.dart';
+
+@immutable
+abstract class SignUpEvent extends Equatable {
+  const SignUpEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class SignUpEmailChanged extends SignUpEvent {
+  final String email;
+
+  const SignUpEmailChanged({required this.email});
+
+  @override
+  List<Object> get props => [email];
+
+  @override
+  String toString() => 'SignUpEmailChanged { email :$email }';
+}
+
+class SignUpPasswordChanged extends SignUpEvent {
+  final String password;
+
+  const SignUpPasswordChanged({required this.password});
+
+  @override
+  List<Object> get props => [password];
+
+  @override
+  String toString() => 'SignUpPasswordChanged { password: $password }';
+}
+
+class SignUpSubmitted extends SignUpEvent {
+  String email;
+  String password;
+
+  SignUpSubmitted({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() => 'SignUpSubmitted { email: $email, password: $password }';
+
+}
+
+class SignUpWithEmailPasswordPressed extends SignUpEvent {
+  final String email;
+  final String password;
+
+  const SignUpWithEmailPasswordPressed(
+      {required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() =>
+      'SignUpWithEmailPasswordPressed { email: $email, password: $password }';
+}
+
+class LogInWithGooglePressed extends SignUpEvent {
+  @override
+  String toString() => 'LogInWithGooglePressed';
+}
