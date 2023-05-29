68867e1012d10ca545f75c3dacd42fb6071788ff
Talhanaeem <talhanaeem347@gmail.com>
2023-05-29 09:57:53 +0500

    searchBloc and searchRepository created

--- lib/bloc/search/search_bloc.dart
+++ lib/bloc/search/search_bloc.dart
@@ -13,9 +13,43 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
   SearchBloc({required SearchRepository searchRepository})
       : _searchRepository = searchRepository,
         super(SearchInitialState()) {
+    on<SelectUserEvent>(_mapSelectUserEventToState);
+    on<PassUserEvent>(_mapPassEventToState);
+    on<LoadUserEvent>(_mapLoadUserEventToState);
+
     on<SearchEvent>(_mapEventToState);
   }
-  void _mapEventToState(event, emit){
 
+  void _mapEventToState(event, emit) {}
+
+  void _mapSelectUserEventToState(event, emit) async {
+    emit(SearchLoadingState());
+    final user = await _searchRepository.chosenUser(
+        currentUserId: event.currentUserId,
+        selectedUserId: event.selectedUserId,
+        name: event.name,
+        photoUrl: event.photoUrl);
+    final currentUser =
+        await _searchRepository.getUserInterests(event.currentUserId);
+    emit(SearchUserState(user: user, currentUser: currentUser));
+  }
+
+  void _mapPassEventToState(event, emit) async {
+    emit(SearchLoadingState());
+    final user = await _searchRepository.passUser(
+      currentUserId: event.currentUserId,
+      selectedUserId: event.selectedUserId,
+    );
+    final currentUser =
+        await _searchRepository.getUserInterests(event.currentUserId);
+    emit(SearchUserState(user: user, currentUser: currentUser));
+  }
+
+  void _mapLoadUserEventToState(event, emit) async {
+    emit(SearchLoadingState());
+    final user = await _searchRepository.getUser(event.userId);
+    final currentUser =
+        await _searchRepository.getUserInterests(event.currentUserId);
+    emit(SearchUserState(user: user, currentUser: currentUser));
   }
 }
--- lib/repositories/search_repository.dart
+++ lib/repositories/search_repository.dart
@@ -1,8 +1,95 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:ideal_playground/models/user.dart';
 
 class SearchRepository {
   final FirebaseFirestore _fireStore;
+
   SearchRepository({FirebaseFirestore? fireStore})
       : _fireStore = fireStore ?? FirebaseFirestore.instance;
+
+  Future<UserModel> chosenUser(
+      {required String currentUserId,
+      required String selectedUserId,
+      required String name,
+      required String photoUrl}) async {
+    await _fireStore
+        .collection('users')
+        .doc(currentUserId)
+        .collection('chosenList')
+        .doc(selectedUserId)
+        .set({});
+    await _fireStore
+        .collection('users')
+        .doc(selectedUserId)
+        .collection('chosenList')
+        .doc(currentUserId)
+        .set({});
+    await _fireStore
+        .collection('users')
+        .doc(selectedUserId)
+        .collection('selectedList')
+        .doc(currentUserId)
+        .set({'name': name, 'photoUrl': photoUrl});
+    return await getUser(currentUserId);
+  }
+
+  Future<UserModel> passUser({
+    required String currentUserId,
+    required String selectedUserId,
+  }) async {
+    await _fireStore
+        .collection('users')
+        .doc(currentUserId)
+        .collection('chosenList')
+        .doc(selectedUserId)
+        .set({});
+    await _fireStore
+        .collection('users')
+        .doc(selectedUserId)
+        .collection('chosenList')
+        .doc(currentUserId)
+        .set({});
+
+    return await getUser(currentUserId);
+  }
+
+  Future<UserModel> getUserInterests(String userId) async {
+    DocumentSnapshot snapshot =
+        await _fireStore.collection('users').doc(userId).get();
+    return UserModel.fromMap(snapshot.data() as Map<String, dynamic>);
+  }
+
+  Future<List<String>> getChosenList(String userId) async {
+    List<String> chosenList = [];
+    QuerySnapshot<Map<String, dynamic>> docs = await _fireStore
+        .collection('users')
+        .doc(userId)
+        .collection('chosenList')
+        .get();
+    for (var doc in docs.docs) {
+      if (doc.exists) {
+        chosenList.add(doc.id);
+      }
+    }
+    return chosenList;
+  }
+
+  Future<UserModel> getUser(String userId) async {
+    final List<String> chosenList = await getChosenList(userId);
+    final UserModel currentUser = await getUserInterests(userId);
+
+    QuerySnapshot<Map<String, dynamic>> users =
+        await _fireStore.collection('users').get();
+
+    for (var doc in users.docs) {
+      if (!chosenList.contains(doc.id) &&
+          doc.id != userId &&
+          currentUser.interestedIn == doc["gender"] &&
+          currentUser.gender == doc["interestedIn"]) {
+        return UserModel.fromMap(doc.data());
+      }
+    }
+
+    return UserModel();
+  }
 }
